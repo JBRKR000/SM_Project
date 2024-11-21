8c21391caceb9c8aa64b44e12db86379396bfff3
Mikolaj Kwiatkowski <jestemczarny2137@gmail.com>
2024-11-21 13:13:22 +0100

    1st commit

---
+++ src/test/java/pbs/edu/cooperative/CooperativeApplicationTests.java
@@ -0,0 +1,13 @@
+package pbs.edu.cooperative;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CooperativeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
